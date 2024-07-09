b88aa9e7ef08ed2cd117b1f06d6a4024c92a6f10
gabrielcapuia <gabriel@fitforbrands.com>
2024-07-09 18:51:33 -0300

    Socket + Handlebars

--- src/app.js
+++ src/app.js
@@ -1,18 +1,47 @@
 import express from "express";
 import productRouter from "./router/product.router.js";
 import cartRouter from "./router/cart.router.js";
+import viewsRouter from "./router/views.router.js";
+import handlebars from "express-handlebars";
+import __dirname from "./dirname.js";
+import { Server } from "socket.io";
 
 const app = express();
+
+// set up Handlebars
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+
 app.use(express.json()); // este middleware perite obtener archivos JSON
+
+// Middleware para permitir CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Permitir solicitudes desde localhost:8080
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+
 app.use("/api", productRouter);
 app.use("/api", cartRouter);
+app.use("/", viewsRouter);
+app.use(express.static("public")); //SI QUIERO VER DESDE PUBLIC CON STATIC va arriba
 
 const PORT = 8080;
 
 // PORT listen
 
-app.listen(PORT, () => {
+const httpServer = app.listen(PORT, () => {
   console.log("puerto conectado: 8080");
+});
+
+export const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  console.log("Nuevo cliente conectado");
 });
---
+++ src/router/views.router.js
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import productManager from "../managers/productManager.js";
+import { io } from "../app.js";
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+  const products = await productManager.getProducts();
+
+  res.render("home", { products });
+});
+
+router.get("/realtimeproducts", async (req, res) => {
+  const products = await productManager.getProducts();
+
+  io.on("connection", (socket) => {
+    console.log("Nuevo cliente conectado");
+    socket.emit("products", products);
+  });
+
+  res.render("realTimeProducts");
+});
+
+router.post("/realtimeproducts", async (req, res) => {
+  console.log(req.body);
+  await productManager.addProduct(req.body);
+  const products = await productManager.getProducts();
+  io.emit("products", products);
+
+  res.render("realTimeProducts");
+});
+
+router.delete("/realtimeproducts", async (req, res) => {
+  const { id } = req.body;
+  console.log(id);
+  await productManager.deleteProduct(id);
+  const products = await productManager.getProducts();
+  io.emit("products", products);
+  res.render("realTimeProducts");
+});
+
+export default router;
